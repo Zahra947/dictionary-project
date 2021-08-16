659aa26db2184e1234c952114903099895dbee94
Zahra947 <zhr.rabiei@gmail.com>
2021-08-16 11:18:06 +0430

    added a component
    this component provides a search engine

---
+++ src/Dictionary.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Dictionary.css";
+
+export default function Dictionary() {
+  //this state will save the submitted data to keep track of it
+  let [keyword, setKeyword] = useState("");
+
+  function search(event) {
+    event.preventDefault();
+  }
+
+  function handleKeyword(event) {
+    //to find where is the data of onChange called : console.log(event.target.value)
+    setKeyword(event.target.value);
+  }
+  return (
+    <form onSubmit={search}>
+      <input type="search" onChange={handleKeyword}></input>
+    </form>
+  );
+}
