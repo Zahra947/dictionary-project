09a65d5112d623a7e2dd299a02279d2ef96263cf
Zahra947 <zhr.rabiei@gmail.com>
2021-08-16 19:20:16 +0430

    Rendered the word's meaning

--- src/Results.js
+++ src/Results.js
@@ -1,8 +1,20 @@
 import React from "react";
+import Meaning from "./Meaning";
 
 export default function Results(props) {
-  if (props.results) {
-    return <div></div>;
+  if (props.definition) {
+    return (
+      <div className="result">
+        <h2>{props.definition.word}</h2>
+        {props.definition.meaning.map(function (meaning, index) {
+          return (
+            <div key={index}>
+              <Meaning meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
   } else {
     return null;
   }
