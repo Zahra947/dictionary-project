59111a4995df626a69bfb8d800ffc36fcb842965
Zahra947 <zhr.rabiei@gmail.com>
2021-08-17 12:57:36 +0430

    added a new component, fix the bug
    a new component is for phonetics of a word

--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Meaning from "./Meaning";
+import Phonetics from "./Phonetics";
 
 export default function Results(props) {
-  if (props.definition) {
+  //console.log(props.results);
+  if (props.results) {
     return (
       <div className="result">
-        <h2>{props.definition.word}</h2>
-        {props.definition.meaning.map(function (meaning, index) {
+        <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetic={phonetic} />
+            </div>
+          );
+        })}
+
+        {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
               <Meaning meaning={meaning} />
